e3eb2eb0bd77553b3e6b271fd5397ba2b117f7d4
Jungseok Hong <ssjason.hong@gmail.com>
2024-06-18 12:00:39 -0400

    classnames fixed

--- yolov8_detector.py
+++ yolov8_detector.py
@@ -152,19 +152,19 @@ class ClosedSetDetector:
         classNames = [
                 "person", "bicycle", "car", "motorcycle", "airplane",
                 "bus", "train", "truck", "boat", "traffic light",
-                "fire hydrant", "stop sign", "parking meter", "benchhhh", "bird",
+                "fire hydrant", "stop sign", "parking meter", "bench", "bird",
                 "cat", "dog", "horse", "sheep", "cow",
                 "elephant", "bear", "zebra", "giraffe", "backpack",
                 "umbrella", "handbag", "tie", "suitcase", "frisbee",
                 "skis", "snowboard", "sports ball", "kite", "baseball bat",
                 "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle",
-                "wine glass", "cupppp", "fork", "knife", "spoon",
+                "wine glass", "cup", "fork", "knife", "spoon",
                 "bowl", "banana", "apple", "sandwich", "orange",
                 "broccoli", "carrot", "hot dog", "pizza", "donut",
                 "cake", "chair", "couch", "potted plant", "bed",
                 "dining table", "toilet", "tv", "laptop", "mouse",
                 "remote", "keyboard", "cell phone", "microwave", "oven",
-                "toaster", "sink", "refrigerator", "bookkk", "clock",
+                "toaster", "sink", "refrigerator", "book", "clock",
                 "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
         ## once classnames are updated, it can be turned into string
         class_names_string = ", ".join(classNames)
