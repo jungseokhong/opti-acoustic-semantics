ebd653637549f9fee074b6be02a5cdd4a091c14e
Ran Choi <crarararan@gmail.com>
2024-09-04 16:52:31 -0400

    remove a tag if it is empty

--- vlm_filter_utils.py
+++ vlm_filter_utils.py
@@ -14,7 +14,7 @@ class vision_filter(PrefixProto):
    You are an assistant that identifies incorrect tags. You respond according to the given steps.
    Step 1. Verify that each tag matches the object in its bounding box.
        example 1:
-       Tag 1 (bag): Correct.
+       Tag 1 (bag): Incorrect. It is empty
        Tag 4 (apple): Incorrect. It contains <object name>
        Tag 5 (apple): Correct 
        Tag 6 (soccer ball): Correct
@@ -23,14 +23,15 @@ class vision_filter(PrefixProto):
        
    Step 2. Determine if there are multiple tags pointing to the same object among the tags identified as correct in Step 1. Return Tags [number of multiple tags]. If there are no multiple tags for one object, return "no multiple tag".
        example 1:
-       Tags[6, 7] : ball, soccer ball are visually pointing to the same object, which is the red ball under the desk
+       Tags[6, 7] : ball, soccer ball are visually pointing to the same object, which is a blue ball under the desk . <describe the look of an object>
    
    Step 3. If there are multiple tags for one object from the response of Step 2, identify which tag is the most accurate. Choose one most precise tag. 
        example 1:
        Tag [6]: object name "soccer ball" is more precise. So, precise_tag = [7]
 
-   Step 4. Provide a summary of the results from Steps 1 and 3 in the following format : <incorrect | corrected | duplicated>_tags = []. Return <incorrect | corrected | duplicated>_tags = [] if No tag.
+   Step 4. Provide lists, <empty | incorrect | corrected | duplicated>_tags =[], results from Steps 1 and 3. [] if No tag.
        example 1:
+       empty_tags = [1]
        incorrect_tags = [4, 11] 
        corrected_tags = ['<object name>', '<object name>']
        duplicated_tags = [6] 
